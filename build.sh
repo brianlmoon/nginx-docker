249df488439a761c5fe6c7912bd744b0fef51aac
Brian Moon <brian@moonspot.net>
2025-09-05 13:56:56 -0500

    Update build to build more versions and not taga major release

--- build.sh
+++ build.sh
@@ -5,28 +5,32 @@ set -e
 function build() {
     IMAGE=$1
 
-    echo "Building $IMAGE"
-
     VERSION=`docker run --rm --entrypoint='' nginx:$IMAGE nginx -v 2>&1 | awk -F / '{print $2}'`
+
+    echo "Building $VERSION"
 
     TAGS=""
 
     TAGS="$TAGS -t brianlmoon/nginx:"`echo $VERSION | awk -F . {'print $1"."$2"."$3'}`
     TAGS="$TAGS -t brianlmoon/nginx:"`echo $VERSION | awk -F . {'print $1"."$2'}`
-    TAGS="$TAGS -t brianlmoon/nginx:"`echo $VERSION | awk -F . {'print $1'}`
 
     if [ "$2" == "latest" ]
     then
         TAGS="$TAGS -t brianlmoon/nginx:latest"
     fi
 
-    docker buildx build \
-        --platform linux/arm/v6,linux/amd64 \
+    CMD="docker buildx build \
+        --platform linux/arm64,linux/amd64 \
         -t brianlmoon/nginx:$IMAGE \
         $TAGS \
         --build-arg BASEIMAGE=$IMAGE \
         --push \
-        .
+        ."
+
+    $CMD
 }
 
-build 1.27-alpine latest
+build 1.26-alpine
+build 1.27-alpine
+build 1.28-alpine
+build 1.29-alpine latest
